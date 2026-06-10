247504a295b83aab85ebf5ce829ebf542c664233
Tatsumi1710 <danglh07.ptit@gmail.com>
2026-06-10 22:49:45 +0700

    fix: serve static frontend

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,11 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
+ // đặt SAU routes API
+app.use(express.static(path.join(__dirname, '../project')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server đang chạy tại port ${PORT}`));
